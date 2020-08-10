e04aacd943404b7b465a4f218216cdb29ae3cf98
Adam Palmer <theadampalmer@gmail.com>
2020-08-10 00:52:06 +0000

    Automatically backed up by Learn

---
+++ lib/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE series (id INTEGER PRIMARY KEY, title TEXT, author_id, subgenre_id);
+CREATE TABLE subgenres (id INTEGER PRIMARY KEY, name text);
+CREATE TABLE authors (id INTEGER PRIMARY KEY, name TEXT);
+CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT, year INTEGER, series_id);
+CREATE TABLE characters (id INTEGER PRIMARY KEY, name TEXT, motto TEXT, species TEXT, author_id);
+CREATE TABLE character_books (id INTEGER PRIMARY KEY, book_id, character_id); 
